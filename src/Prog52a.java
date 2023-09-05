b1499e9fe8729bf098a98144e0fded010fd6270f
Jacob Slinde <slinde.j@stu.janesville.k12.wi.us>
2023-09-05 10:46:48 -0500

    Initial commit

---
+++ src/Prog52a.java
@@ -0,0 +1,18 @@
+public class Prog52a {
+    public static void main(String[] args) {
+        int length = 143;
+        int width = 80;
+        width = 82;
+
+        // + - * / %(remainder)
+        int area = length * width;
+        int perim = 2 * length + 2 * width;
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perim);
+
+    }
+}
+/*
+Area = 11726
+Perimeter = 450
+ */
