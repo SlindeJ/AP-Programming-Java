6276a0e0fcc0b3a08fd02764bfd3891c6e7787fe
SlindeJ <slinde.j@stu.janesville.k12.wi.us>
2023-09-08 11:24:51 -0500

    Update

---
+++ src/Q1/Prog58h.java
@@ -0,0 +1,14 @@
+package Q1;
+import java.util.Scanner;
+
+public class Prog58h {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Amount saved: ");
+        double p = input.nextDouble();
+        System.out.print("Interest rate? ");
+        double r = input.nextDouble();
+
+
+    }
+}
