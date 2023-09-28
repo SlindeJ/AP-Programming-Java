4643fc2bd78065e42313d09a4c53ebdaea5a46c9
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-09-28 12:39:09 -0500

    Update

--- src/Q1/Prog122i.java
+++ src/Q1/Prog122i.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Prog122i {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        for (int lcv = -25; lcv < 26; lcv++) {
+            
+        }
     }
 }
