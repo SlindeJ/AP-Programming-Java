d8f36e9f8975225a05b407c3e11c6e9996465704
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-10-10 10:58:51 -0500

    Update

--- src/Q1/Prog122d.java
+++ src/Q1/Prog122d.java
@@ -1,19 +1,19 @@
-package Q1;
-
-public class Prog122d {
-    public static void main(String[] args) {
-        int x = -12;
-        while (x < 17) {
-            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
-<<<<<<< HEAD
-            System.out.println(x + "\t\t\t\t" + y);
-            x++;
-=======
-            System.out.println(x + "\t\t" + y);
-            x++;
-        }
->>>>>>> 2107944 (Update over weekend)
-
-        }
-    }
-}
+//package Q1;
+//
+//public class Prog122d {
+//    public static void main(String[] args) {
+//        int x = -12;
+//        while (x < 17) {
+//            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+//<<<<<<< HEAD
+//            System.out.println(x + "\t\t\t\t" + y);
+//            x++;
+//=======
+//            System.out.println(x + "\t\t" + y);
+//            x++;
+//        }
+//>>>>>>> 2107944 (Update over weekend)
+//
+//        }
+//    }
+//}
