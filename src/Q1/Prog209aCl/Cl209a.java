654d742c1c8e8305da597ff5c6c87b57611ac46c
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-10-20 11:24:24 -0500

    Update

--- src/Q1/Prog209aCl/Cl209a.java
+++ src/Q1/Prog209aCl/Cl209a.java
@@ -4,23 +4,35 @@ public class Cl209a {
     private int rnum;    // number that was read in
     private int G500; // greater than 500
     private int L500; // less than 500
-    private int tnums; // total read numbers
+    //private int tnums; // total read numbers
 
 
     public Cl209a(int numi) {   // number in
         rnum = numi;
         G500 = 0;
         L500 = 0;
-        tnums += rnum;      // may need += tnums
+        //tnums += rnum;      // may need += tnums
     }
 
-    public void calc() {
-        if (rnum >= 500) {
-            G500++;
-        } else { L500++; }
+//    public void calc() {
+//        if (rnum >= 500) {
+//            G500++;
+//        } else { L500++; }
+//    }
+
+
+
+
+    // DOING BOOLEANS!!!!!
+    public boolean greater(int num) {
+        return (num >= 500);
+    }
+    public boolean lesser(int num) {
+        return (num < 500);
     }
 
-    public int getTotal() { return tnums; }
-
+    public int getTotal() { return G500 + L500; }
+    public int getG500() { return G500; }
+    public int getL500() { return L500; }
 
 }
--- src/Q1/Prog209aCl/Prog209aCl.java
+++ src/Q1/Prog209aCl/Prog209aCl.java
@@ -8,11 +8,19 @@ public class Prog209aCl {
     public static void main(String[] args) {
     try {
         Scanner input = new Scanner(new File("Langdat/prog215a.dat")); // remember to replace with new data file
-
+        int num = 0;
         while (input.hasNext()) {
-
+            num = input.nextInt();
+            Cl209a helper = new Cl209a(num);
+            //helper.calc();
 
         }
+        // test to find out where file needs to be imported
+        Cl209a helper = new Cl209a(0);
+        int great = helper.getG500();
+        int less = helper.getL500();
+        int summ = helper.getTotal();
+        System.out.println(summ);
     } catch (IOException e) {
         System.out.println("Can't find data file!");
     }
