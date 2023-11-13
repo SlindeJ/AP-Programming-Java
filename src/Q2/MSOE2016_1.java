c10fcce2e10820971a55a1b6292340aaca91dcbc
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-11-13 11:25:14 -0600

    Update

---
+++ src/Q2/MSOE2016_1.java
@@ -0,0 +1,14 @@
+package Q2;
+import java.util.Scanner;
+
+public class MSOE2016_1 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in); // input
+        System.out.println("Please enter a lowercase word: ");
+        String word = scnr.nextLine();
+        for (int lcv = word.length()-1; lcv > 0; lcv -=1) {
+
+        }
+
+    }
+}
