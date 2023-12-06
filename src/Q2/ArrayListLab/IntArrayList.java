0778ddb2b4c662b78eb40d21fea651e8e11795df
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-12-06 11:37:44 -0600

    Update

--- src/Q2/ArrayListLab/IntArrayList.java
+++ src/Q2/ArrayListLab/IntArrayList.java
@@ -11,5 +11,37 @@ public class IntArrayList {
         mySize = 0;
         myCapacity = DEFAULT_SIZE;
     }
+    private void doubleCapacity() { //=> doubles the capacity of myArray
+        myCapacity *= 2;
+        int[] tempArray = new int[myCapacity];
+        for (int lcv = 0; lcv < mySize - 1; lcv++) {
+            tempArray[lcv] = myArray[lcv];
+        }
+
+    }
+    private void halfCapacity() { // => halves the capacity of myArray
+
+    }
+    private void add(int num) { // => adds the provided number to the end of myArray
+
+    }
+    public void remove(int index) { // => removes the item at the provided index and shifts all items down
+
+    }
+    public int get(int index) { // => returns the item at the provided index
+
+    }
+    public void set(int index, int num) { // => set the provided index to the provided number
+
+    }
+    public int size() { // => returns mySize
+
+    }
+    public int[] toArray() { //=> returns myArray;
+
+    }
+    public int indexOf(int num) { // => returns the index of the provided number in myArray
+
+    }
 
 }
