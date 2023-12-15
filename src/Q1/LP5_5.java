ac28126c21a487b686f6d158707eb969d73e37c9
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2023-12-15 20:50:28 +0000

    Holy cow

--- src/Q1/LP5_5.java
+++ src/Q1/LP5_5.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package Q1;
 //<<<<<<< HEAD
 //import java.util.*;
@@ -33,3 +34,41 @@
 //3
 // */
 //>>>>>>> f645ff9 (weekend update)
+=======
+package CodeSpaces.Q1;
+// package Q1;
+// <<<<<<< HEAD
+// import java.util.*;
+// =======
+// import java.util.Scanner;
+// >>>>>>> f645ff9 (weekend update)
+
+// public class LP5_5 {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+// <<<<<<< HEAD
+//         System.out.print("Please enter a positive number: ");
+//         int num1 = input.nextInt();
+
+//     }
+// }
+// =======
+//         System.out.print("Please enter the number: ");
+//         int number = input.nextInt();
+
+//         while (number > 0) {
+//             System.out.println(number % 10);
+//             number = (int)(number / 10);
+//         }
+        
+// //     }
+    
+// }
+// /*
+//  * Please enter the number: 356
+// 6
+// 5
+// 3
+//  */
+// >>>>>>> f645ff9 (weekend update)
+>>>>>>> e691320 (Holy cow)
