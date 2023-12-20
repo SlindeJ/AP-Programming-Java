44872619167245be5d60851d0dae58ddfe624540
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2023-12-20 11:24:06 -0600

    Update

--- src/Q1/Prog122d.java
+++ src/Q1/Prog122d.java
@@ -1,86 +1,86 @@
-<<<<<<< HEAD
 ////<<<<<<< HEAD
-////package Q1;
-////
-////public class Prog122d {
-////    public static void main(String[] args) {
-////        int x = -12;
-////        while (x < 17) {
-////            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
-////<<<<<<< HEAD
-////            System.out.println(x + "\t\t\t\t" + y);
-////            x++;
+////////<<<<<<< HEAD
+//////package Q1;
+//////
+//////public class Prog122d {
+//////    public static void main(String[] args) {
+//////        int x = -12;
+//////        while (x < 17) {
+//////            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+//////<<<<<<< HEAD
+//////            System.out.println(x + "\t\t\t\t" + y);
+//////            x++;
+//////=======
+//////            System.out.println(x + "\t\t" + y);
+//////            x++;
+//////        }
+//////>>>>>>> 2107944 (Update over weekend)
+//////
+//////        }
+//////    }
+//////}
 ////=======
-////            System.out.println(x + "\t\t" + y);
-////            x++;
-////        }
-////>>>>>>> 2107944 (Update over weekend)
+////// package Q1;
 ////
-////        }
-////    }
-////}
+////// public class Prog122d {
+//////     public static void main(String[] args) {
+//////         int x = -12;
+//////         while (x < 17) {
+//////             double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+////// <<<<<<< HEAD
+//////             System.out.println(x + "\t\t\t\t" + y);
+//////             x++;
+////// =======
+//////             System.out.println(x + "\t\t" + y);
+//////             x++;
+//////         }
+////// >>>>>>> 2107944 (Update over weekend)
+////
+//////         }
+//////     }
+////// }
+////>>>>>>> 1b1e106 (update)
 //=======
-//// package Q1;
-//
-//// public class Prog122d {
-////     public static void main(String[] args) {
-////         int x = -12;
-////         while (x < 17) {
-////             double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+//package CodeSpaces.Q1;
 //// <<<<<<< HEAD
-////             System.out.println(x + "\t\t\t\t" + y);
-////             x++;
+//// //package Q1;
+//// //
+//// //public class Prog122d {
+//// //    public static void main(String[] args) {
+//// //        int x = -12;
+//// //        while (x < 17) {
+//// //            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+//// //<<<<<<< HEAD
+//// //            System.out.println(x + "\t\t\t\t" + y);
+//// //            x++;
+//// //=======
+//// //            System.out.println(x + "\t\t" + y);
+//// //            x++;
+//// //        }
+//// //>>>>>>> 2107944 (Update over weekend)
+//// //
+//// //        }
+//// //    }
+//// //}
 //// =======
-////             System.out.println(x + "\t\t" + y);
-////             x++;
-////         }
-//// >>>>>>> 2107944 (Update over weekend)
+//// // package Q1;
 //
-////         }
-////     }
-//// }
-//>>>>>>> 1b1e106 (update)
-=======
-package CodeSpaces.Q1;
-// <<<<<<< HEAD
-// //package Q1;
-// //
-// //public class Prog122d {
-// //    public static void main(String[] args) {
-// //        int x = -12;
-// //        while (x < 17) {
-// //            double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
-// //<<<<<<< HEAD
-// //            System.out.println(x + "\t\t\t\t" + y);
-// //            x++;
-// //=======
-// //            System.out.println(x + "\t\t" + y);
-// //            x++;
-// //        }
-// //>>>>>>> 2107944 (Update over weekend)
-// //
-// //        }
-// //    }
-// //}
-// =======
-// // package Q1;
-
-// // public class Prog122d {
-// //     public static void main(String[] args) {
-// //         int x = -12;
-// //         while (x < 17) {
-// //             double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
-// // <<<<<<< HEAD
-// //             System.out.println(x + "\t\t\t\t" + y);
-// //             x++;
-// // =======
-// //             System.out.println(x + "\t\t" + y);
-// //             x++;
-// //         }
-// // >>>>>>> 2107944 (Update over weekend)
-
-// //         }
-// //     }
-// // }
-// >>>>>>> 1b1e106 (update)
->>>>>>> e691320 (Holy cow)
+//// // public class Prog122d {
+//// //     public static void main(String[] args) {
+//// //         int x = -12;
+//// //         while (x < 17) {
+//// //             double y = Math.pow(x, 6) - (3 * Math.pow(x, 5)) - (93 * Math.pow(x, 4)) + (87 * Math.pow(x, 3)) + (1596 * Math.pow(x, 2)) - (1380 * x) - 2800;
+//// // <<<<<<< HEAD
+//// //             System.out.println(x + "\t\t\t\t" + y);
+//// //             x++;
+//// // =======
+//// //             System.out.println(x + "\t\t" + y);
+//// //             x++;
+//// //         }
+//// // >>>>>>> 2107944 (Update over weekend)
+//
+//// //         }
+//// //     }
+//// // }
+//// >>>>>>> 1b1e106 (update)
+//>>>>>>> e691320 (Holy cow)
--- src/Q2/BigArrayListCat.java
+++ src/Q2/BigArrayListCat.java
@@ -65,7 +65,17 @@ public class BigArrayListCat {
             }
             System.out.println();
 //                    Remove all cats under $26. Print the costs of each cat remaining on the list.
-
+            for (int lcv = 0; lcv < cats.size(); lcv++) {
+                if (cats.get(lcv).getCost() < 26) {
+                    cats.remove(lcv);
+                    lcv--;
+                }
+            }
+            System.out.println("\n11. The costs of the remaining cats are: ");
+            for (Cat cat : cats) {
+                System.out.print(cat.getCost() + " ");
+            }
+            System.out.println();
 //            All cats heavier than 15 pounds need to go on a diet <--  no for-each this time.
 //                    Print the names of the cats being put on a diet.
             System.out.println("\n12. The cats being put on a diet are: ");
