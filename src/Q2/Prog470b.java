08df26e3da193a6bdd9c640aad77a07c511da317
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2024-01-18 15:39:12 +0000

    update space

--- src/Q2/Prog470b.java
+++ src/Q2/Prog470b.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Prog470a {
+public class Prog470b {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("Langdat/prog464a.dat")); // remember to replace with new data file
