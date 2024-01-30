6f05aed9d27a90d132c8b27a224164bb90c2055d
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-01-30 11:19:17 -0600

    Update

--- src/Q3/Prog465h.java
+++ src/Q3/Prog465h.java
@@ -25,7 +25,7 @@ public class Prog465h {
                     }
                 }
                 //
-                System.out.println();
+                System.out.println("Original Matrix");
                 for (int[] row : mat) {
                     for (int n : row) {
                         System.out.print(n + " ");
@@ -34,7 +34,7 @@ public class Prog465h {
                 }
                 //
                 if ((count * 3) > (r * c)) {
-                    // original matrix is abundant
+                    System.out.println("The Original Matrix is Abundant\n");
                 }
                 else if ((count * 3) <= (r * c)) {
 
@@ -46,6 +46,19 @@ public class Prog465h {
                         if (mat[row][col] != 0) { rmat[rrow][2] = mat[row][col]; rmat[rrow][0] = row+1; rmat[rrow][1] = col+1; rrow++; } 
                         }
                     }
+                    //
+                    for (int[] row : rmat) {
+                        for (int n : row) {
+                            System.out.print(n + " ");
+                        }
+                        System.out.println();
+                    }
+                    if ((count * 3) == (r * c)) {   //
+                        System.out.println("The Original Matrix and the Sparse Matrix\n" +
+                                "are Equally Efficient");
+                    } else {
+                        System.out.println("The Original Matrix is Sparse\n");
+                    }
                     // rmat made
                 } // do also equal here probably
             }
@@ -55,3 +68,42 @@ public class Prog465h {
         }
     }
 }
+/*
+Original Matrix
+0 0 7 0 0 0
+0 0 0 0 -8 0
+0 0 0 0 0 0
+2 0 0 0 0 0
+0 0 0 0 0 0
+1 3 7
+2 5 -8
+4 1 2
+The Original Matrix is Sparse
+
+Original Matrix
+0 2 0 3 0 1
+8 0 4 0 1 0
+0 3 0 1 0 -7
+5 0 9 0 6 0
+0 2 0 -1 0 7
+The Original Matrix is Abundant
+
+Original Matrix
+0 0 1 0 0 2
+3 0 0 4 0 0
+0 0 5 0 0 6
+7 0 0 8 0 0
+0 0 9 0 0 1
+1 3 1
+1 6 2
+2 1 3
+2 4 4
+3 3 5
+3 6 6
+4 1 7
+4 4 8
+5 3 9
+5 6 1
+The Original Matrix and the Sparse Matrix
+are Equally Efficient
+ */
