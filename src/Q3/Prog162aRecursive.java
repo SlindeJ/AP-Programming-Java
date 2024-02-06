47efaa41a3f0b0dc8d7167331a8002eb106ecc15
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-02-06 11:07:19 -0600

    Update

---
+++ src/Q3/Prog162aRecursive.java
@@ -0,0 +1,36 @@
+package Q3;
+import java.util.*;
+
+public class Prog162aRecursive {
+    public static int factLoop(int n) {
+        int product = 1;
+        for (int i = 1; i <= n; i++)
+            product *= i;
+        return product;
+    }
+
+    public static int factRevLoop(int n) {
+        int product = 1;
+        for (int i = n; i > 0; i--) {
+            product *= 1;
+        }
+        return product;
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) { return 1; }           // base case
+        return n * factorial(n-1);       // recursive case
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        while (num != 0) {
+            int fact = factorial(num);
+            System.out.println(num + "! = " + fact);
+            System.out.print("Enter a number: ");
+            num = input.nextInt();
+        }
+    }
+}
---
+++ src/Q3/Prog477a.java
@@ -0,0 +1,47 @@
+package Q3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;
+
+public class Prog477a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog477a.dat")); // remember to replace with new data file
+            int[][] mat = new int[5][3];
+//            for (int row = 0; row < mat.length; row++) {
+//                for (int col = 0; col < mat[0].length; col++) {
+//                    mat[row][col] = input.nextInt(); }
+//            }
+            // maybe enhanced for loop?
+//            int count = 0;
+//            for (int[] row : mat) {
+//                for (int n : row) {
+//                    if (n != 0) { count++; }
+//                }
+//            }
+            int totMale = 0;
+            int totFemale = 0;
+            while (input.hasNext()) {
+                int type = input.nextInt();
+                int grade = input.nextInt();
+                mat[grade-1][type-1]++;
+                mat[grade-1][2]++;
+                if (type == 1) { totMale++; }
+                if (type == 2) { totFemale++; }
+            }
+            System.out.println("Grade\tMale\tFemale\tTotal");
+            for (int[] row : mat) {
+                System.out.print("ABCDF ");
+                for (int n : row) {
+                    System.out.print(n + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("Totals:\t" + totMale + "\t" + totFemale + "\t" + (totFemale+totMale));
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
