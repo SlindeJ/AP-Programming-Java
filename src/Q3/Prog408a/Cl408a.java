ed7aaa26a76fe41689bad6a5890a5d57e3d9434d
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-02-12 11:24:39 -0600

    Update

---
+++ src/Q3/Prog408a/Cl408a.java
@@ -0,0 +1,19 @@
+package Q3.Prog408a;
+
+public class Cl408a {
+    private int _ID;
+    private int _Score;
+
+    public Cl408a(int id, int score) {
+        _ID = id;
+        _Score = score;
+    }
+
+    public int getID() {
+        return _ID;
+    }
+
+    public int getScore() {
+        return _Score;
+    }
+}
--- src/Q3/Prog607a.java
+++ src/Q3/Prog607a.java
@@ -8,7 +8,8 @@ public class Prog607a {
     // year Z-A (Z is 1) + 1970
     public static String deCode(String code) {
         String month = code.substring(0, 1); 
-        String day = code.substring(1, 3); 
+        String day1 = code.substring(1, 2);
+        String day2 = code.substring(2, 3);
         String year = code.substring(3);
 
         // "ABCDEFGHIJKL"
@@ -17,16 +18,42 @@ public class Prog607a {
         // Z = 1, A = 26
         String[] years = {"Z" ,"Y" ,"X" ,"W" ,"V" ,"U" ,"T" ,"S" ,"R" ,"Q" ,"P" ,"O" ,"N" ,"M" ,"L" ,"K" ,"J" ,"I" ,"H" ,"G" ,"F" ,"E" ,"D" ,"C" ,"B" ,"A"};
         for (int i = 0; i < months.length; i++) {
-            if (months[i].equals(months)) { month = toString(i+1); } // fix this
+            if (months[i].equals(month)) { month = "" + (i+1); } // bruh
         }
-        return month + " " + day + " " + year; 
+        for (int i = 0; i < dates.length; i++) {
+            if (dates[i].equals(day1)) { day1 = "" + (i+1); }
+        }
+        for (int i = 0; i < dates.length; i++) {
+            if (dates[i].equals(day2)) { day2 = "" + (i+1); }
+        }
+        for (int i = 0; i < years.length; i++) {
+            if (years[i].equals(year)) { year = "" + (i+1 + 1970); } // fix this
+        }
+        return month + " " + day1 + day2 + " " + year;
     }
     public static String enCode(String code) {
         String[] arr = code.split("/", 3);
         String month = arr[0];
-        String day = arr[1];
+        String day1 = arr[1].substring(0, 1);
+        String day2 = arr[1].substring(1);
         String year = arr[2];
-        return month + " " + day + " " + year;
+
+        String[] months = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+        String[] dates = {"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        // Z = 1, A = 26
+        String[] years = {"Z" ,"Y" ,"X" ,"W" ,"V" ,"U" ,"T" ,"S" ,"R" ,"Q" ,"P" ,"O" ,"N" ,"M" ,"L" ,"K" ,"J" ,"I" ,"H" ,"G" ,"F" ,"E" ,"D" ,"C" ,"B" ,"A"};
+
+        // use .valueOf(String s);
+        // do here
+        int m = Integer.valueOf(month) - 1;
+        int d1 = Integer.valueOf(day1) - 1;
+        int d2 = Integer.valueOf(day2) - 1;
+        int y = Integer.valueOf(year) - 1;
+        month = months[m];
+        day1 = dates[d1];
+        day2 = dates[d2];
+        year = years[y];
+        return month + " " + day1 + day2 + " " + year;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
