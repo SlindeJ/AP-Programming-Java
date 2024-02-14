66c218470bbfecd31fd0cdd10a67ea422786a024
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-02-14 11:27:44 -0600

    Update

--- src/Q3/Prog703s/Prog703s.java
+++ src/Q3/Prog703s/Prog703s.java
@@ -20,42 +20,55 @@ public class Prog703s {
                 int type = input.nextInt();
                 String name = input.next();
                 int number = input.nextInt();
-                double value = input.nextInt();
+                double value = input.nextDouble();
                 if (type == 1) {
                     String color = input.next();
                     Computer v = new Apple(name, number, value, color);
                     list.add(v);
-                }
-                else if (type == 2) {
+                } else if (type == 2) {
                     double version = input.nextDouble();
                     Computer v = new Windows(name, number, value, version);
                     list.add(v);
-                }
-                else if (type == 3) {
+                } else if (type == 3) {
                     int code = input.nextInt();
                     Computer v = new Linux(name, number, value, code);
                     list.add(v);
                 }
-
+            }
                 int GoG = 0; // Green or Gold (apple computers)
-                int ver7 = 0; // windows on ver. 7
+                int ver7 = 0; // windows on ver. 7.0
                 int endin5 = 0; // linux codes that end in 5
                 //
                 for (Computer v : list) {
                     if (v instanceof Apple) {
-                        if (((Apple)v).getColor().equals("Green") || GoG == 1){ // fix
-
+                        if (((Apple)v).getColor().equals("Green") || ((Apple)v).getColor().equals("Gold")){
+                            GoG++;
                         }
                     }
                     if (v instanceof Windows) {
+                        if (((Windows)v).getVersion() == 7.0) {
+                            ver7++;
+                        }
                     }
                     if (v instanceof Linux) {
+                        if (((Linux)v).getCode() % 10 == 5) {
+                            endin5++;
+                        }
                     }
                 }
                 //
-            }
+
+                System.out.println("There are " + GoG + " green or gold Apple computers");
+                System.out.println("There are " + ver7 + " Windows computers on version 7.0");
+                System.out.println("There are " + endin5 + " Linux computers with a code that ends in a 5");
+
         } catch (IOException e) {
             System.out.println("Can't find data file!");
         }
     }
 }
+/*
+There are 1 green or gold Apple computers
+There are 2 Windows computers on version 7.0
+There are 4 Linux computers with a code that ends in a 5
+ */
