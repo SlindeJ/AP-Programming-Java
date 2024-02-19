732ad77b6b8a90c99605895a58ac186f8e91a35c
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-02-19 11:13:45 -0600

    Update

---
+++ src/Q3/Farm/Cow.java
@@ -0,0 +1,18 @@
+package Q3.Farm;
+
+public class Cow extends Animal {
+    private static final double MILK_PRICE_LB = 0.20;   // static makes it across all cows and never changes
+    private double myMilk;
+
+    public Cow(String name, int weight, int corn, int hay, double milk) {
+        super(name, weight, corn, hay);
+        myMilk = milk;
+    }
+
+    public double value(double cornCost, double hayCost) {
+        return myMilk * MILK_PRICE_LB - getFeedCost(cornCost, hayCost);
+    }
+
+    public double getMilk()             { return myMilk; }
+    public static double getMilkPrice() { return MILK_PRICE_LB; }
+}
