769e002b7964bd8dc95c3176e853f61b3039f762
slinde.j <slinde.j@stu.janesville.k12.wi.us>
2024-02-22 11:23:16 -0600

    Update

--- src/Q3/Farm1/Farm.java
+++ src/Q3/Farm1/Farm.java
@@ -1,4 +1,12 @@
 package Q3.Farm1;
 
-public class Farm implements IFarm {
+import java.util.ArrayList;
+
+public abstract class Farm implements IFarm {
+    private ArrayList<Horse> myHorses;
+    private ArrayList<Cow> myCows;
+    private int myNumHayBales;
+    private int myNumCorn;
+    private double myHayCost;
+    private double myCornCost;
 }
--- src/Q3/Farm1/IFarm.java
+++ src/Q3/Farm1/IFarm.java
@@ -36,16 +36,3 @@ public interface IFarm {
     public ArrayList<Horse> getHorses();
 }
 
-
-
-public class Farm implements IFarm {
-    private ArrayList<Horse> myHorses;
-    private ArrayList<Cow> myCows;
-    private int myNumHayBales;
-    private int myNumCorn;
-    private double myHayCost;
-    private double myCornCost;
-
-    // …
-
-}
