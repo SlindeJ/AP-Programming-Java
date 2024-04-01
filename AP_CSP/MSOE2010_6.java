94be19227db25e52d816a88263c4d9dc63374d1f
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2024-04-01 14:38:58 +0000

    codespace update

---
+++ AP_CSP/MSOE2010_6.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+
+
+public class MSOE2010_6 {
+    public int summation(int verts, int x, int y) {
+        if (verts == 1) { return }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        System.out.println("How many vertices are there? ") 
+        int vertices = input.nextInt(); 
+
+
+    }
+}
+
+
