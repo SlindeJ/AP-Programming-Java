70213f547a854b1a25582ead53b5343120b965fb
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2024-04-04 14:40:05 +0000

    update

--- AP_CSP/MSOE2015_4.java
+++ AP_CSP/MSOE2015_4.java
@@ -8,6 +8,17 @@ public class MSOE2015_4 {
         for (int i = 0; i < 10; i++) {
             System.out.print("Number " + (i+1) + ": "); 
             arr[i] = input.nextDouble(); 
+            if (i >= 2) {
+                
+                double[] uniquearr = Arrays.sort(arr); 
+                for (int k = 0; k < uniquearr.length-1; k++) {
+                    if (uniquearr[k] == uniquearr[k+1]) {
+                        // need to remove item at index k here
+                    k--; } }
+                int third = uniquearr.length-3; 
+                if (third < 0) third = 0; 
+                System.out.println(uniquearr[third]); 
+            }
         }
         
     }
