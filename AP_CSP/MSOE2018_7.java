e7cddbc3b994e1cee8c6bdced330fdde53b79e47
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2024-04-10 14:33:44 +0000

    update

--- AP_CSP/MSOE2018_7.java
+++ AP_CSP/MSOE2018_7.java
@@ -10,15 +10,25 @@ public class MSOE2018_7 {
         int SecInYear = 31_536_000; 
         int SecInDay = 86_400; 
         Scanner input = new Scanner(System.in); 
-        System.out.print("Enter UNIX time: ") 
+        System.out.print("Enter UNIX time: "); 
         int unix = input.nextInt(); 
         int year = 1970; 
         int month = 1; 
-        int day = 1; 
         year += notMod(SecInYear, unix);
         unix = unix % SecInYear; 
         // account for leap year somewhere around here
-        int days = notMod(SecInDay, unix); 
-        
+        int days = notMod(SecInDay, unix) + 1; 
+        // days in months = 31 28/29 31 30 31 30 31 31 30 31 30 31
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+        for (int i = 0; i < months.length; i++) {
+            if (days > months[i]) {
+                days -= months[i]; 
+                month++; 
+            } else {
+                break; 
+            }
+        }
+        // leap year here
+        System.out.println(month + "/" + days + "/" + year);
     }
 }
