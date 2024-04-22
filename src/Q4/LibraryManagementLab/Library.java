2b367ff277e31dbc1181e4ee0b0dfa4e9d9556aa
SlindeJ <slinde.j@stu.janesville.k12.wi.us>
2024-04-22 11:20:30 -0500

    Update

--- src/Q4/LibraryManagementLab/Library.java
+++ src/Q4/LibraryManagementLab/Library.java
@@ -70,7 +70,6 @@ public class Library implements LibrarySystem {
     public void viewMostRecentTransaction(String isbn) {
         // Hint: Use a backward loop to find the most recent transaction
         // If no transaction is found, print "No transactions found for ISBN: <isbn>"
-        // checking to see if commit works
     }
 
     // TODO: Complete the implementation of LibrarySystem methods
