3b421e6ff75c2b5b594abeb4f83aafbc582d2c04
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2024-04-30 13:41:02 +0000

    update

--- src/Q4/LibraryManagementLab/BinarySearchUtil.java
+++ src/Q4/LibraryManagementLab/BinarySearchUtil.java
@@ -1,7 +1,21 @@
 package Q4.LibraryManagementLab;
 
 public class BinarySearchUtil {
-    // TODO: Implement binary search method (takes in ArrayList) using recursion for searching books by title (required) or author
-    //
-    
+    public Book binarySearch(ArrayList<Book> arr, Book b) {
+        int l = 0; 
+        int r = arr.size() - 1; 
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr.get(m) == b) {
+            return b;
+        }
+        else if (arr.get(m).getTitle().compareTo(b.getTitle()) < 0) {   // may or may not be the right direction
+            l = m + 1;
+        }
+        else {
+            r = m - 1;
+        } 
+    }
+    return -1;
+    } 
 }
