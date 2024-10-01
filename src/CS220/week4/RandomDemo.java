a78bff4dd37905342ca9d7e3dda585451aac8b11
SlindeJ <swimninja23@gmail.com>
2024-09-30 20:37:10 -0500

    update

--- src/CS220/week4/RandomDemo.java
+++ src/CS220/week4/RandomDemo.java
@@ -1,6 +1,6 @@
-
-
-	/**
+import java.util.Random;
+import java.util.*;
+/**
 	   This program prints ten random numbers by using Math.random() method
 	   https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Math.html#random()
 When Math.random() is first called, it creates a single new pseudorandom-number generator, 
@@ -25,7 +25,30 @@ exactly as if by the expression new java.util.Random()
 	         System.out.print(die + "\t");
 	      }
 	      System.out.println();
-	    
+
+		  //
+		   int times = 10;
+		   Random rGenerator = new Random();
+		   int[] die1 = new int[times], die2 = new int[times];
+		   for (int i = 0; i < times; i++) {
+			   die1[i] = rGenerator.nextInt(6) + 1;
+			   die2[i] = rGenerator.nextInt(6) + 1;
+		   }
+		   System.out.println("Die1: " + Arrays.toString(die1));
+		   System.out.println("Die2: " + Arrays.toString(die2));
+
+		   int[] countForDie1 = new int[6];
+		   int[] countForDie2 = new int[6];
+
+		   // count frequency
+		   for (int i = 0; i < times; i++) {
+			   countForDie1[die1[i]-1]++; // get the face number from die1 and die2
+			   countForDie2[die2[i]-1]++;
+		   }
+		   System.out.printf("\n%7s%10s%10s\n", "face", "Die1count", "Die2count");
+		   for (int i = 0; i < 6; i++) {
+			   System.out.printf("\n%7d%10d%10d\n", (i+1), countForDie1[i], countForDie2[i]);
+		   }
 	   }
 	}
 
