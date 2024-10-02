d7b857af281b027f79a575d9d467404d12a8028d
SlindeJ <swimninja23@gmail.com>
2024-10-02 18:23:50 -0500

    update

--- src/CS220/ESPGame.java
+++ src/CS220/ESPGame.java
@@ -11,6 +11,7 @@ public class ESPGame {
         // colors are red, green, blue, yellow, purple
         int[] colors = new int[5];
         int[] randColors = new int[5];
+        int rand;
         String output = "";
         while (true) {
             String input = (String) JOptionPane.showInputDialog(null, "Enter a color: r(red), g(green),b(blue), y(yellow), p(purple):\nEnter q to quit", "ESP game", JOptionPane.QUESTION_MESSAGE); // Initial choice is "Red" here
@@ -27,13 +28,29 @@ public class ESPGame {
                 break;
                 case "p": colors[4]++;
                 break;
+                default: System.exit(0);
+            }
+            rand = returnRand();
+            randColors[rand]++;
+            switch (rand) {
+                case 0: System.out.println("The computer chose: red"); break;
+                case 1: System.out.println("The computer chose: green"); break;
+                case 2: System.out.println("The computer chose: blue"); break;
+                case 3: System.out.println("The computer chose: yellow"); break;
+                case 4: System.out.println("The computer chose: purple"); break;
                 default: break;
             }
-            System.out.println(input);
-            randColors[returnRand()]++;
         }
+        System.out.println();
+        System.out.println("Your guesses:");
         printGuesses(colors);
+        System.out.println("The computer's guesses: ");
         printGuesses(randColors);
+        int total = 0;
+        for (int i : randColors) {
+            total += i;
+        }
+        System.out.println("Total games played: " + total);
     }
 
     public static int returnRand() {    // returns int between 0 - 4
--- src/CS220/week5/AccountTest.java
+++ src/CS220/week5/AccountTest.java
@@ -27,26 +27,39 @@ public class AccountTest{
 				
 		//(4) create an array of three checking accounts
 		Account[] checkingAccounts = new Account[3];
-		for (int i = 0; i < checkingAccounts.length; i++) {
-			checkingAccounts[i] = new Account();
-			System.out.println(checkingAccounts[i]);
-		}
+//		for (int i = 0; i < checkingAccounts.length; i++) {
+//			checkingAccounts[i] = new Account();
+//			System.out.println(checkingAccounts[i]);
+//		}
+		checkingAccounts[0] = new Account("sss", 500);
+		checkingAccounts[1] = new Account("aaa", 500);
+		checkingAccounts[2] = new Account("bbb", 500);
+		printAccounts(checkingAccounts, "Checking Account");
 		// create an account array and provide account objects by calling constructor,
 		// how many Account objects we assigned to this array will determine the size of the array
 		Account[] checkingAccountsV2 = new Account[] {new Account(), new Account("Bob", 1000), new Account("Sam", 400) };
+		printAccounts(checkingAccountsV2, "Checking Account");
 		//(5) call printAccounts() method to print out all checking accounts info.
 
 				
 		//(6) create an ArrayList of 10 saving accounts
-				
-				
+		ArrayList<Account> savingAccounts = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			savingAccounts.add(new Account());
+		}
 		//(7) call printAccounts() method to print out all saving accounts info.
-				
+		Account[] savingListToArray = new Account[savingAccounts.size()];
+		savingListToArray = savingAccounts.toArray(savingListToArray);
+		// convert ArrayList to be array and then print them out by calling printAccounts()
+		printAccounts(savingListToArray, "Saving Account");
 		
 		
 	}
 	// private means printAccounts() is only used inside AccountTest
-	public static void printAccounts() {
-
+	public static void printAccounts(Account[] checkingAccountsV2, String type) {
+		System.out.println("Print " + type + " :");
+		for (Account c : checkingAccountsV2 ) {
+			System.out.println(c);
+		}
 	}
 }
