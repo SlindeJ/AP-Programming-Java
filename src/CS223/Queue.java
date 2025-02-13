4be1471633ebf0cc1b79af15c9cde34acf9bd3e3
SlindeJ <swimninja23@gmail.com>
2025-02-13 16:58:03 -0600

    update

--- src/CS223/Queue.java
+++ src/CS223/Queue.java
@@ -40,8 +40,27 @@ public class Queue {
     public int Size(){
         return size;
     }
+
+    private String FArray(String[] arr, int front, int rear, int size) {
+        String arrString = "";
+        while (front != rear) {
+            arrString += STR."\{arr[front++]}, ";
+            if (front == max_size) { front = 0; }
+        }
+        if (size == max_size) {     // This is disgusting
+            for (int i = 0; i < size; i++) {
+                arrString += STR."\{arr[front++]}, ";
+                if (front == max_size) { front = 0; }
+            }
+        }
+
+
+        if (arrString.length() > 1) { return "[" + arrString.substring(0, arrString.length() - 2) + "]"; }
+        else { return "[]"; }
+    }
+
     public void print_Queue() {
-        System.out.printf("%-40s %-10s %-10s %-10s \n", Arrays.toString(q), front, rear, size);       // q, front, rear, size
+        System.out.printf("%-40s %-10s %-10s %-10s \n", FArray(q, front, rear, size), front, rear, size);       // q, front, rear, size
         // had to use google to remind myself how to format the spacing again
     }
 }
